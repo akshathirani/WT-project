c292bed5666340500f380d14620e1dd665e2cd4f
akshathirani <hakshat04@gmail.com>
2021-09-19 14:05:08 +0530

    search console

--- main.js
+++ main.js
@@ -43,3 +43,15 @@ $.getJSON("https://newsapi.org/v2/top-headlines?country=us&apiKey=f13a081f035740
         
     });
 })
+
+function yo(){
+fetch("https://newsapi.org/v2/everything?q=" +document.getElementById("query").value + "&from="+document.getElementById("date")+"&to="+document.getElementById("date")+"&sortBy=" +document.getElementById("exampleFormControlSelect1")+"&apiKey=f13a081f035740b1967300a8357a3e69")
+.then(res=>res.json())
+.then((data)=>{
+  console.log(data);
+  console.log(document.getElementById("date").value)
+  
+}
+  
+)
+}
