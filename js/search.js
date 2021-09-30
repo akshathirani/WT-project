8a492fbd86c1a2108856c7a8d0c761e32d9cce02
akshathirani <hakshat04@gmail.com>
2021-09-30 08:09:53 +0530

    index

--- js/search.js
+++ js/search.js
@@ -3,7 +3,44 @@ function search(){
     .then(res=>res.json())
     .then((data)=>{
       console.log(data);
-      console.log(document.getElementById("date").value)
+      const sr = data.articles.slice(0,6);
+      var i=0;
+      sr.forEach(element => {
+        if(element.urlToImage == null) {
+          const srcard = `
+                      <div class="col-lg-4 col-md-4 col-sm-12">
+                        <div class="srcards card bg-dark text-white">
+                          <img class="card-img" src="https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg" alt="image">
+                          <div class="card-img-overlay">
+                            <h5 class="card-title srcards_head">${element.title}</h5>
+                            <p class="card-text srcards_des">${element.description}</p>
+                            <button type="button" class="btn btn-outline-success view_btn">View</button>
+                          </div>
+                        </div>
+                      </div>
+                    
+          `
+          i=i+1;
+          document.getElementById("search_result").innerHTML += srcard;
+        }
+        else {
+          const srcard = `
+                        <div class="col-lg-4 col-md-4 col-sm-12 c_smtg">
+                          <div class="srcards card bg-dark text-white">
+                            <img class="card-img" src="${element.urlToImage}"" alt="image">
+                            <div class="card-img-overlay">
+                              <h5 class="card-title srcards_head">${element.title}</h5>
+                              <p class="card-text srcards_des">${element.description}</p>
+                              <button type="button" class="btn btn-outline-success view_btn">View</button>
+                            </div>
+                          </div>
+                        </div>
+          `
+          i=i+1;
+          document.getElementById("search_result").innerHTML += srcard;
+        }
+      });
       }
     )
+    .catch(err => console.log(err));
 }
